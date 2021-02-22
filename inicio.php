a3ad22ad233d1b993e008d36fc2e74ba41a1ba84
isivroag <isivroag@gmail.com>
2021-02-22 16:33:30 -0600

    beta1

--- inicio.php
+++ inicio.php
@@ -27,6 +27,7 @@
   <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
@@ -114,7 +115,7 @@
         <div class="carousel-inner" role="listbox">
 
           <!-- Slide 1 -->
-          <div class="carousel-item active" style="background-image: url('assets/img/slide/slide-1.jpg');">
+          <div class="carousel-item active" style="background-image: url('assets/img/slide/slide-4.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
                 <!--<h2 class="animate__animated animate__fadeInDown">Bienvenido <span>Grupo AQUAX</span></h2>-->
@@ -125,7 +126,7 @@
           </div>
 
           <!-- Slide 2 -->
-          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-2.jpg');">
+          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-5.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
                 <h2 class="animate__animated animate__fadeInDown"></h2>
@@ -136,7 +137,29 @@
           </div>
 
           <!-- Slide 3 -->
-          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-3.jpg');">
+          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-10.jpg');">
+            <div class="carousel-container">
+              <div class="carousel-content container">
+                <h2 class="animate__animated animate__fadeInDown"></h2>
+                <p class="animate__animated animate__fadeInUp"></p>
+                <!--<a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>-->
+              </div>
+            </div>
+          </div>
+
+           <!-- Slide 3 -->
+           <div class="carousel-item" style="background-image: url('assets/img/slide/slide-7.jpg');">
+            <div class="carousel-container">
+              <div class="carousel-content container">
+                <h2 class="animate__animated animate__fadeInDown"></h2>
+                <p class="animate__animated animate__fadeInUp"></p>
+                <!--<a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>-->
+              </div>
+            </div>
+          </div>
+
+           <!-- Slide 3 -->
+           <div class="carousel-item" style="background-image: url('assets/img/slide/slide-8.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
                 <h2 class="animate__animated animate__fadeInDown"></h2>
@@ -383,36 +406,30 @@
       <div class="container">
         <div class="mx-auto d-flex justify-content-center">
           <div class="row text-light justify-content-center mx-auto">
-            <div class="col-lg-1">
-              <i class=" fas fa-file-alt fa-6x"></i>
+            <div class="col-lg-3 d-flex ">
+              <i class=" fas fa-file-invoice-dollar fa-6x mx-3"></i>
+              <h5 class="mx-3 my-auto text-light">CURSOS ADECUDADOS PARA TI</h5>
+              
             </div>
 
-            <div class="col-lg-2">
-              <h4>CURSOS ADECUDADOS PARA TI</h4>
+            <div class="col-lg-3 d-flex">
+              <i class=" fas fa-swimmer fa-6x"></i>
+              <h5 class="mx-3 my-auto text-light">28 AÑOS DE EXPERIENCIA</h5>
             </div>
+        
 
-            <div class="col-lg-1">
-              <i class=" fas fa-file-alt fa-6x"></i>
+            <div class="col-lg-3 d-flex">
+              <i class=" fas fa-users fa-6x"></i>
+              <h5 class="mx-3 my-auto text-light">GRUPOS REDUCIDOS</h5>
             </div>
+      
+
+            <div class="col-lg-3 d-flex">
+              <i class=" fas fa-book-reader fa-6x"></i>
+              <h5 class="mx-3 my-auto text-light">INSTRUCTORES CAPACITADOS CONTINUAMENTE</h5>
+            </div>
+  
             
-            <div class="col-lg-2">
-              <h4>28 AÑOS DE EXPERIENCIA</h4>
-            </div>
-
-            <div class="col-lg-1">
-              <i class=" fas fa-file-alt fa-6x"></i>
-            </div>
-            
-            <div class="col-lg-2">
-              <h4>GRUPOS REDUCIDOS</h4>
-            </div>
-
-            <div class="col-lg-1">
-              <i class=" fas fa-file-alt fa-6x"></i>
-            </div>
-            
-            <div class="col-lg-2">
-              <h4>INSTRUCTORES CAPACITADOS CONTINUAMENTE</h4>
             </div>
 
           </div>
@@ -550,73 +567,57 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <!--<footer id="footer">
+  <footer id="footer">
     <div class="footer-top">
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>Mamba</h3>
-            <p>
-              A108 Adam Street <br>
-              NY 535022, USA<br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+          <div class="col-lg-2 col-md-6 footer-info ">
+            <img class="logofooter"src="assets/img/logow.png" alt="">
+            
+          </div>
+          <div class="col-lg-3 ">
+          <div class="social d-flex align-self-center align-items-center ">
+
+          <a href="#" class="icono "><img class="iredes" src="assets/img/facebookw.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/instagramw.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/correow.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/whatsappw.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/telefonow.png"></img></a>
             </div>
           </div>
 
           <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
+            
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">SERVICIOS</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">SUCURSALES</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">NOSOTROS</a></li>
+            
             </ul>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
+           
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#"> COVID-19 </a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">REGLAMENTO</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">CONTACTO</a></li>
+            
             </ul>
           </div>
 
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-
-          </div>
 
         </div>
       </div>
-    </div>-->
+    </div>
 
   <div class="container">
     <div class="copyright">
       &copy; Copyright <strong><span>TECNIEM</span></strong>. Todos los derechos reservados.
     </div>
     <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+     
     </div>
   </div>
   </footer><!-- End Footer -->
@@ -638,6 +639,7 @@
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
+  <script src="assets/js/segundo.js"></script>
 
 </body>
 
