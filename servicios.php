7fbd6b25a37f95eea9ade3ddc01b6770cd7bee07
isivroag <isivroag@gmail.com>
2021-03-04 17:05:16 -0600

    primeras secciones

---
+++ servicios.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Grupo Aquax</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
+
+  <link href="//db.onlinewebfonts.com/c/67137b5f6928edc941186baaab5a664b?family=Lemon/Milk" rel="stylesheet" type="text/css" />
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Mamba - v2.5.1
+  * Template URL: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container aviso">
+      <div class="row justify-content-end">
+        <div class="col-md-6 text-right">
+          NUESTRAS MEDIDAS SANITARIAS
+          <a href="index.html"><img src="assets/img/covid19.png" alt="" class="img-fluid covid" style="max-width:100px;border-radius: 60px;"></a>
+
+        </div>
+        <div class="col-md-3">
+
+          <a href="#" class="icono"><img class="iredes" src="assets/img/facebook.png"></img></a>
+          <a href="#" class="icono"><img class="iredes" src="assets/img/instagram.png"></img></a>
+          <a href="#" class="icono"><img class="iredes" src="assets/img/correo.png"></img></a>
+          <a href="#" class="icono"><img class="iredes" src="assets/img/whatsapp.png"></img></a>
+          <a href="#" class="icono"><img class="iredes" src="assets/img/telefono.png"></img></a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container">
+
+      <div class="logo float-left">
+        <!--<h1 class="text-light"><a href="index.html"><span>Mamba</span></a></h1>-->
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <a href="index.html"><img src="assets/img/logo2.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu float-right d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Inicio</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#portfolio">Sucursales</a></li>
+          <li><a href="#about">Nosotros</a></li>
+          <li><a href="#team">Reglamento</a></li>
+          
+          <!--<li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>-->
+          </li>
+          <li><a href="#contact">Contacto</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+<section id="titulo" class="tituloplan">
+    <div class="container">
+    <div class="row d-flex justify-content-between ">
+        <div class="col-md-7 my-auto">
+            <div class="contenedortitulo">
+            <h2 class="nombretitulo">AQUAMATERNAL</h2>
+            <H4 class="subtitulo text-center">GRUPO AQUAX</H4>
+            </div>
+          
+        </div>
+        <div class="col-md-3 ">
+            <img class="align-top imagenplan" src="assets/img/30min.png" alt="">
+        </div>
+    </div>
+    </div>
+    
+</section>
+
+
+<section>
+<div class="container">
+    <div class="row d-flex justify-content-between">
+        <div class="detlletexto col-lg-4 offset-1 my-auto">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, dolore. Odit atque nulla 
+                officia molestias voluptatibus est, laboriosam distinctio ipsa repellat quam dignissimos harum sed asperiores animi iure facere laudantium?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, dolore. Odit atque nulla 
+                officia molestias voluptatibus est, laboriosam distinctio ipsa repellat quam dignissimos harum sed asperiores animi iure facere laudantium?
+            </p>
+        </div>
+        <div class="col-lg-5">
+            <img class="detalleplan" src="assets/img/aquamat.png" alt="">
+        </div>
+    </div>
+</div>
+</section  >
+
+<section class="plan">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <img src="assets/img/planaaquamat.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+</section>
+
+
+
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-2 col-md-6 footer-info ">
+            <img class="logofooter"src="assets/img/logow.png" alt="">
+            
+          </div>
+          <div class="col-lg-3 ">
+          <div class="social d-flex align-self-center align-items-center ">
+
+          <a href="#" class="icono "><img class="iredes" src="assets/img/facebookw.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/instagramw.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/correow.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/whatsappw.png"></img></a>
+          <a href="#" class="icono "><img class="iredes" src="assets/img/telefonow.png"></img></a>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">SERVICIOS</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">SUCURSALES</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">NOSOTROS</a></li>
+            
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+           
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#"> COVID-19 </a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">REGLAMENTO</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">CONTACTO</a></li>
+            
+            </ul>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong><span>TECNIEM</span></strong>. Todos los derechos reservados.
+    </div>
+    <div class="credits">
+     
+    </div>
+  </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script src="assets/js/segundo.js"></script>
+
+</body>
+
+</html>
